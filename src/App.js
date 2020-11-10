5c99017d03e0dbdf11f2ee5f271c4a9da41ad47b
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-11-10 15:33:26 -0500

    began routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import './App.css';
-import PrLogContainer from './containers/PrLogContainer'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+
+import PracticeLog from './components/PracticeLog'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <main>
+                  <Navigation />
+
+          <Switch>
+          <Route path='/' component={Home} />
+          <Route path='/PracticeLog' component={PracticeLog} />
+
+          </Switch>
+      </main>
+  )
+  }
+}
+
+export default App;
+
+{/* <div className="App">
         <header className="App-header">
           <div >
           <h1 className="app-name">Musica<b>Practica</b></h1>
@@ -19,8 +38,4 @@ class App extends React.Component {
         </main>
 
       </div>
-    );
-  }
-}
-
-export default App;
+    ); */}
---
+++ src/components/PracticeLog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PrLogContainer from '../containers/PrLogContainer'
+
+class PracticeLog extends React.Component {
+
+
+render() {
+    return (
+
+        <div>
+          <PrLogContainer />
+        </div> 
+    )
+
+}
+
+
+}
+
+export default PracticeLog
